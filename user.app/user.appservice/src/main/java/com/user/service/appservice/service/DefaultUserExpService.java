e08e5148117e66c65c7969e7eeb6d7545c6978e3
MagicG <pangdachong@gmail.com>
2018-10-23 13:32:28 +0800

    init

--- user.app/user.appservice/src/main/java/com/user/service/appservice/service/DefaultUserExpService.java
+++ user.app/user.appservice/src/main/java/com/user/service/appservice/service/DefaultUserExpService.java
@@ -1,4 +1,32 @@
 package com.user.service.appservice.service;
 
+import com.user.service.dbservice.domain.UserExp;
+import com.user.service.dbservice.service.PLUserExpService;
+
+import java.util.List;
+
 public class DefaultUserExpService implements UserExpService {
+    private PLUserExpService service;
+
+    /**
+     * 新增一条记录
+     *
+     * @param exp 记录
+     * @return
+     */
+    @Override
+    public int create(UserExp exp) {
+        return service.create(exp);
+    }
+
+    /**
+     * 查询用户对应的所有记录
+     *
+     * @param userid
+     * @return
+     */
+    @Override
+    public List<UserExp> queryByUserID(String userid) {
+        return service.queryByUserID(userid);
+    }
 }
--- user.app/user.appservice/src/main/java/com/user/service/appservice/service/UserExpService.java
+++ user.app/user.appservice/src/main/java/com/user/service/appservice/service/UserExpService.java
@@ -1,4 +1,26 @@
 package com.user.service.appservice.service;
 
+import com.user.service.dbservice.domain.UserExp;
+
+import java.util.List;
+
+/**
+ * APP服务
+ */
 public interface UserExpService {
+    /**
+     * 新增一条记录
+     *
+     * @param exp 记录
+     * @return
+     */
+    int create(UserExp exp);
+
+    /**
+     * 查询用户对应的所有记录
+     *
+     * @param userid
+     * @return
+     */
+    List<UserExp> queryByUserID(String userid);
 }
