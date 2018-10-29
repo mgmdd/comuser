20864f0bf070f763a184ab0271952995c391b546
MagicG <pangdachong@gmail.com>
2018-10-29 14:02:34 +0800

    修改maven assembly 删除部分无效内容

---
+++ user.common/src/main/java/com/user/service/BeanService.java
@@ -0,0 +1,30 @@
+package com.user.service;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanService {
+    private static ApplicationContext ctx;
+
+    static {
+        try {
+            String[] cfgs = {"spring/spring_appservice.xml", "spring/spring_dbservice.xml"};
+
+            ctx = new ClassPathXmlApplicationContext(cfgs);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static <T> T getService(String id) {
+        return (T) ctx.getBean(id);
+    }
+
+    public static <T> T getService(Class<T> clz) {
+        return ctx.getBean(clz);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ user.common/src/test/java/com/user/service/BeanServiceTest.java
@@ -0,0 +1,13 @@
+package com.user.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BeanServiceTest {
+
+    @Test
+    public void getService() {
+//        BeanService.getService(UserService.class)
+    }
+}
